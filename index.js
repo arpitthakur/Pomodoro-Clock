634aefd772d0e78da64de18ede62a0c5c21e4782
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-22 11:22:46 +0530

    added function alertMsg

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const plusBtn = document.getElementById('plus-btn');
 const minusBtn = document.getElementById('minus-btn');
+let status = document.getElementById('status');
+const playBtn = document.getElementById('play-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const stopBtn = document.getElementById('stop-btn');
+const alarmAudio = doccument.getElementById('myAudio');
+let remainingSeconds;
+let currentState="stop";
+let interval;
+function alertMsg () {
+    alert('Invalid Input, Please Enter only digits than zero');
+}
